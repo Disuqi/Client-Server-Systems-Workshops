e3659308c6f57239ae506312c9c9900b6f6d9a43
Disuqi <disuqi.hijazi.7@gmail.com>
2021-10-13 14:22:44 +0100

    added ex5 and questions to ask

--- questionIWantToAsk.md
+++ questionIWantToAsk.md
@@ -1,3 +1,4 @@
+# Questions I want to ask in this week's workshop
 1. Check ex1() is this the best way to do it? Is there a more efficient way? In python you can do [0-5] or something like that to get the first 5 numbers is there something similar in php.
-2. Have I dont ex3() correctly? Why did we need to use dechex()? I did not use it and it still works.
+2. Have I done ex3() correctly? Why did we need to use dechex()? I did not use it and it still works.
 
--- workshop3.php
+++ workshop3.php
@@ -1,7 +1,8 @@
 <?php
 ex1();
 ex2(10);
-ex3(1000, 10000);
+ex3(1000, 1000);
+ex4($_POST['factorial']);
 function ex1(){
     $recordedTemps = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
     sort($recordedTemps);
@@ -44,5 +45,12 @@ function ex3($n, $m){
     for($i=$n;$i<$m;$i++){
         echo "Icon for 0x". $i ." is " . '&#' . $i . '<br>';
     }
+}
+function ex4($n){
+    $result=1;
+    for($i=$n; $i > 0; $i--){
+        $result *= $i;
+    }
+    echo $n . " factorial is " . $result;
 }
 
